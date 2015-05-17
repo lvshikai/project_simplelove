5f93aa1ae5883b3bb4bdb82fa9f51ab3ef8261b9
greensky123 <lsklsk123>
2015-05-17 18:02:35 +0800

    fix jar包的重复，
    建了welcome

---
+++ 简爱/src/com/example/easylove/activity/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.easylove.activity;
+import com.example.easylove.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+}
+}
